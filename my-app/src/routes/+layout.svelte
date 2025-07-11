1f6f5969f802d67088d3b3ee7ff23f6a22f490f1
Rafsneto <rafaoneto@gmail.com>
2025-07-11 11:56:55 -0300

    front inicializado

--- my-app/src/routes/+layout.svelte
+++ my-app/src/routes/+layout.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
 	import '../app.css';
-	import Nav from './Components/Nav.svelte';
 
 	let { children } = $props();
 </script>
 
 {@render children()}
 
-<Nav/>
--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,7 +1,8 @@
+<script lang="ts">	import Nav from './Components/Nav.svelte';
+</script>
 <svelte:head>
     <link rel="stylesheet" href="app.css">
 
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
 </svelte:head>
+<Nav />
 
-<h1>Controle de Frequência</h1> <img src="/CP2TudoouNada.png" alt="Emblema do cp2">
